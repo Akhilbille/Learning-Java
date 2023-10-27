aa0892b7313ca99efe759074d1659fbcb6fc3dc1
Akhil Bille <akhilbille@gmail.com>
2023-10-26 20:15:08 -0400

    Operators

---
+++ J4Variables.java
@@ -0,0 +1,16 @@
+public class J4Variables {
+    public static void main(String args[]){
+        byte b = 120;
+        short s = 129;
+        int num = 1231234567;
+        long l = 12322222226l;
+        float f = 12.3f;
+        double d = 12.3;
+        char c = 'A';
+        String str = "Akhil";
+
+        System.out.println("Bytes: " + b);
+        System.out.println("Char: " + (s-c));
+        
+    }
+}
---
+++ J5IntToOtherNum.java
@@ -0,0 +1,9 @@
+public class J5IntToOtherNum {
+    public static void main(String args[]){
+        int x = 64;
+        System.out.println("Integer to Binary: " + Integer.toBinaryString(x));
+        System.out.println("Integer to Hexadecimal: " + Integer.toHexString(x));
+        System.out.println("Integer to Octal: " + Integer.toOctalString(x));
+        System.out.println("Integer to Octal: " + Integer.toString(x,2));
+    }
+}
