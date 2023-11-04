3db40ffdf79475305c3a201d7f9e721234762d40
Akhil Bille <akhilbille@gmail.com>
2023-11-04 10:21:57 -0400

    String Practise

---
+++ J10StringImmutable.java
@@ -0,0 +1,9 @@
+public class J10StringImmutable {
+    public static void main(String[] args){
+        String str = "Java";
+        str = str.toLowerCase();
+        System.out.println(str);
+        
+
+    }
+}
